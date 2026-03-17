59b551ceb6d1e0e21ca01dc227099e4f585e18b9
stitsyuk <stitsyukart@gmail.com>
2026-03-17 19:23:27 +0900

    feat: add local port for frontend

--- aki_frontend/src/hooks/useAPI.tsx
+++ aki_frontend/src/hooks/useAPI.tsx
@@ -6,7 +6,8 @@ import { User } from "@/types/user";
 import { UserResponse } from "@/types/responses";
 
 // const HOME = "/api"; // FOR BUILD ONLY
-const HOME = "http://localhost:9000/api"; // FOR DEV ONLY
+// const HOME = "http://localhost:9000/api"; // FOR DEV ONLY
+const HOME = "http://localhost:8999/api"; // FOR ARTYOM ONLY
 
 const getContextObject = (apiInstance: AxiosInstance = axios) => ({
   get: (route: string, config: AxiosRequestConfig = {}) => apiInstance.get(route, config),
